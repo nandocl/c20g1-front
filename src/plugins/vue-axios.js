89550517e7c4be0877d1c3dc6f08edde75f25beb
nandocl <andresclaro00@gmail.com>
2021-11-27 14:18:36 -0500

    Heroku config

--- src/plugins/vue-axios.js
+++ src/plugins/vue-axios.js
@@ -2,7 +2,8 @@ import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
-const baseURL = "http://localhost:3000";
+// const baseURL = "http://localhost:3000";
+const baseURL = "https://grupo1b20mintic-back.herokuapp.com";
 axios.defaults.baseURL = baseURL;
 
 Vue.use(VueAxios, axios);
